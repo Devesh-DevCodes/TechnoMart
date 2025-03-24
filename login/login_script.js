0517462f08027cb3f8aff5e55d6aa43e78de30b3
Devesh Verma <deveshlucky200@gmail.com>
2025-03-24 20:13:53 +0530

    updattes

--- login/login_script.js
+++ login/login_script.js
@@ -83,33 +83,6 @@ onAuthStateChanged(auth, (user) => {
 });
 
 
-// ---------------------cart icon element count -----------------
-let basket =[];
-let cart_items_count = () => {
-  let cart_icon_items = document.getElementById("cart_number");
-  let count = 0;
-
-  fetch('http://localhost:5500/cart')
-  .then(response => response.json())
-  .then(data => {
-    basket = data;
-    // console.log( 'cart data',data);
-    // console.log( 'cart basket',basket);
-    for(let x of basket){
-      count += Number(x.Quantity);
-    }
-    cart_icon_items.innerHTML = count;
-    console.log( 'total cart count',count);
-  })
-  .catch(error => {
-    console.error('Error fetching cart:', error);
-  });
-  
-};
-cart_items_count();
-
-
-
 
 /*
 
